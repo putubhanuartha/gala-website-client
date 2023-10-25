eb8337e97030dea4c37d641eca038260f18ef009
putubhanuartha <putuaryabhanu03@gmail.com>
2023-10-25 22:38:53 +0700

    fix: "optimize rockin image"

--- app/(home)/components/rockin/image-card.tsx
+++ app/(home)/components/rockin/image-card.tsx
@@ -9,7 +9,10 @@ const ImageCard: React.FC<ImageCardType> = ({ src, title }) => {
 		<div className="relative w-24 h-36 sm:w-32 sm:h-40 md:w-44 md:h-56 lg:w-52 lg:h-64 xl:w-64 xl:h-96 overflow-clip rounded-3xl">
 			<Image
 				fill
+				priority
+				placeholder="blur"
 				src={src}
+				blurDataURL={src}
 				alt={title}
 				sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
 				style={{ objectFit: "cover" }}
